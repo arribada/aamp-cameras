aed73370aed87b2fd29fb0d90a24e4317d6d6752
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-05 08:23:11 +0100

    Access point added

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
-modprobe v4l2_common && python cam-pir.py &
+#!/bin/bash
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+sleep 5
+
+ifconfig wlan1 down
+
+sleep 2
+
+ifconfig wlan1 up
+
+sleep 5
+
+python ./activate_connection.py aamp-camera-5
+
+modprobe v4l2_common && python cam-single-photo.py &
 cd /data
 python -m SimpleHTTPServer 80
