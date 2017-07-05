a072ea40d0e35f014a13c55c509ada7d4194c7ea
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-05 18:25:31 +0100

    Update

--- start.sh
+++ start.sh
@@ -7,7 +7,15 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-sleep 7
+sleep 5
+
+ifconfig wlan1 down
+
+sleep 2
+
+ifconfig wlan1 up
+
+sleep 5
 
 python ./activate_connection.py $ACCESS_POINT_NAME
 
