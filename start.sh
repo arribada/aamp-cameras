f22b861f511c29175ae714860023fd6b99f43e4a
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-05 18:19:18 +0100

    Adding OpenSSH

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+# Start sshd if we don't use the init system
+if [ "$INITSYSTEM" != "on" ]; then
+  /usr/sbin/sshd -p 22 &
+fi
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-sleep 5
-
-ifconfig wlan1 down
-
-sleep 2
-
-ifconfig wlan1 up
-
-sleep 5
+sleep 7
 
 python ./activate_connection.py $ACCESS_POINT_NAME
 
