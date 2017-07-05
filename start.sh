2db677f4a331a7338d6efb74fd6f1eb265846334
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-05 17:21:20 +0100

    Added env var for AP

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ ifconfig wlan1 up
 
 sleep 5
 
-python ./activate_connection.py aamp-camera-5
+python ./activate_connection.py echo $ACCESS_POINT_NAME
 
 modprobe v4l2_common && python cam-single-photo.py &
 cd /data
