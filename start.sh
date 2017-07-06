d6912d9120c39e716ab16fd260ad63e82e6b9ce7
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-06 17:25:40 +0100

    update iptables avahi

--- start.sh
+++ start.sh
@@ -7,6 +7,15 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+set -e
+
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+
+systemctl stop avahi-daemon &&
+
+avahi-daemon
+
 sleep 5
 
 ifconfig wlan1 down
