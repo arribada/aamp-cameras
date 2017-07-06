1c0aac17e303cd87bd0308ee9f4d025f86be7cb7
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-07 00:37:32 +0100

    updated camera script

--- cam-timelapse-photo.py
+++ cam-timelapse-photo.py
@@ -9,5 +9,6 @@ camera = PiCamera()
 camera.resolution = (1920, 1080)
 
 for i in range(720):
-    camera.capture('image{0:04d}.jpg'.format(i))
+    camera.capture('/data/image{0:04d}.jpg'.format(i))
+    print "Photo taken - waiting 2 mins"
     sleep(120)
--- start.sh
+++ start.sh
@@ -7,15 +7,6 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-set -e
-
-# Set host-name and enable-dbus
-sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
-
-systemctl stop avahi-daemon &&
-
-avahi-daemon
-
 sleep 5
 
 ifconfig wlan1 down
@@ -28,6 +19,15 @@ sleep 5
 
 python ./activate_connection.py $ACCESS_POINT_NAME
 
-modprobe v4l2_common && python cam-single-photo.py &
+modprobe v4l2_common && python cam-timelapse-photo.py &
 cd /data
 python -m SimpleHTTPServer 80
+
+set -e
+
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+
+systemctl stop avahi-daemon &&
+
+avahi-daemon
