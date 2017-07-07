60f4cae8ac907e1365b14dffea07911a57ae7aa6
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-07 01:05:51 +0100

    update

--- start.sh
+++ start.sh
@@ -7,14 +7,14 @@ fi
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-set -e
+# set -e
 
 # Set host-name and enable-dbus
-sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+# sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
-systemctl stop avahi-daemon &&
+# systemctl stop avahi-daemon &&
 
-avahi-daemon &
+# avahi-daemon &
 
 sleep 5
 
