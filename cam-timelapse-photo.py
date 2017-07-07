9e0263e168b0c06bc754d6e064436a55155908e1
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-07 08:07:33 +0100

    updates

--- cam-timelapse-photo.py
+++ cam-timelapse-photo.py
@@ -5,6 +5,14 @@ from os import system
 from time import sleep
 from picamera import PiCamera
 
+file_path = "/data/{timestamp:%Y-%m-%d}"
+directory = os.path.dirname(file_path)
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 camera = PiCamera()
 camera.resolution = (1920, 1080)
 
